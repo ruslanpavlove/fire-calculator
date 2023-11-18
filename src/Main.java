34911f5bb6993a21037a33aa85fb46f5368e9cc3
Ruslan Pavlov <ruslanpavlove@gmail.com>
2023-11-18 16:32:26 +0300

    Проверка входных данных

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Год:");
+        String inputYear = scanner.nextLine();
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        try {
+            int startYear = Integer.parseInt(inputYear);
+            if (startYear < 2002 || startYear > 2021) {
+                throw new IllegalArgumentException("throws Exception…");
+            }
+        } catch (NumberFormatException e) {
+            System.out.print("Введи год в диапазоне от 2002 до 2021");
+        } catch (IllegalArgumentException e) {
+            System.out.print(e.getMessage());
         }
     }
 }
